aafefee8187accb8af50ffcbe2e3b247e6059acd
amr9mohamed <amrmohamed53@gmail.com>
2018-12-15 01:04:11 +0200

    automatic intrest point detector return required points, yet to be tested to calculate homography matrix

--- main.py
+++ main.py
@@ -139,6 +139,27 @@ def stitch_images(path_1, path_2, correspondance_points=5, save=True, load=True)
 
     return res
 
+def automatic_intrest_points_detector(image1, image2, N):
+    ###
+    # image1 and image2 are the 2 images that have commen points
+    # N is number of points reqired to be detected
+    ###
+    
+    # ORB: An efficient alternative to SIFT or SURF
+    orb = cv.ORB_create() 
+    
+    # get key points and descriptors from the 2 images
+    kps1, descs1 = orb.detectAndCompute(image1, None)
+    kps2, descs2 = orb.detectAndCompute(image2, None)
+    
+    # brute force matcher
+    bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(descs1, descs2)
+    matches = sorted(matches, key=lambda x:x.distance)
+    list_kps1 = [kps1[mat.queryIdx].pt for mat in matches[:N]] 
+    list_kps2 = [kps2[mat.trainIdx].pt for mat in matches[:N]]
+    
+    return list_kps1, list_kps2
 
 res = stitch_images('b1.png','b2.png' )
 
